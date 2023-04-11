401dbc369d579a1b06f402d19a222965fa0537ec
unknown <dyupina.as@edu.spbstu.ru>
2023-04-11 19:21:24 +0300

    sdf

--- my_sonar_analysis.bat
+++ my_sonar_analysis.bat
@@ -2,9 +2,10 @@ rem "C:\Program Files\dotnet\dotnet.exe" build
 
 
 
-"C:\Program Files\dotnet\dotnet.exe" sonarscanner begin
+"C:\Program Files\dotnet\dotnet.exe" sonarscanner begin /k:"123k" /d:sonar.host.url="http://127.0.0.1:9000"  /d:sonar.login="sqp_2184f1f144696fbb2a68cacf3203818496121fc2"
 "C:\Program Files\dotnet\dotnet.exe" build
-"C:\Program Files\dotnet\dotnet.exe" sonarscanner end
+"C:\Program Files\dotnet\dotnet.exe" sonarscanner end /d:sonar.login="sqp_2184f1f144696fbb2a68cacf3203818496121fc2"
+
 
 
 rem dotnet sonarscanner begin /k:"123k" /d:sonar.host.url="http://127.0.0.1:9000"  /d:sonar.login="sqp_2184f1f144696fbb2a68cacf3203818496121fc2"
